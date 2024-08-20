336e7e2e7c4d915dbec6e48ce40d5e5459a7247c
Juan Pablo Solís Ruiz <A01067387@tec.mx>
2024-08-20 10:21:08 -0600

    Double messenger

--- G4-PCM/include/PrimaryGeneratorAction.hh
+++ G4-PCM/include/PrimaryGeneratorAction.hh
@@ -7,11 +7,16 @@
 #include "globals.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4ParticleGun.hh"
+#include "PrimaryGeneratorMessenger.hh"
+#include "G4UIcmdWithADoubleAndUnit.hh"
+#include "PrimaryGeneratorAction.hh"
+#include "G4RunManager.hh"
 
 
 
 namespace G4_PCM
 {
+	class PrimaryGeneratorMessenger; // Forward declaration
 
 	class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 	{
@@ -20,8 +25,14 @@ namespace G4_PCM
 		~PrimaryGeneratorAction();
 
 		virtual void GeneratePrimaries(G4Event*);
+		void SetGunZpos(G4double zpos);
 
 		G4ParticleGun* fParticleGun;
+	private:
+		G4double fPgun = -5. * cm; // Valor predeterminado
+
+		G4UIcmdWithADoubleAndUnit* fPgunCmd;
+		PrimaryGeneratorMessenger* fMessenger; // Pointer to the messenger
 	};
 }
 
