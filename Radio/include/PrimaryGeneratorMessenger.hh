0c4a6c4241712c5f7ace4d8a0795180cf8b24e1c
Juan Pablo Solís Ruiz <A01067387@tec.mx>
2024-08-26 14:13:27 -0600

    A ver

---
+++ Radio/include/PrimaryGeneratorMessenger.hh
@@ -0,0 +1,23 @@
+#ifndef G4_PCM_PRIMARY_GENERATOR_MESSENGER_H
+#define G4_PCM_PRIMARY_GENERATOR_MESSENGER_H 1
+
+#include "G4UImessenger.hh"
+#include "G4UIcmdWithADoubleAndUnit.hh"
+
+namespace G4_PCM
+{
+    class PrimaryGeneratorAction;
+    class PrimaryGeneratorMessenger : public G4UImessenger
+    {
+    public:
+
+        PrimaryGeneratorMessenger(PrimaryGeneratorAction* gun);
+        ~PrimaryGeneratorMessenger() override;
+        void SetNewValue(G4UIcommand* command, G4String newValue) override;
+    private:
+        PrimaryGeneratorAction* fGun;
+        G4UIcmdWithADoubleAndUnit* fPgunCmd;
+    };
+}
+
+#endif
